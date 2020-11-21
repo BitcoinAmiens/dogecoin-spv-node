d05b331ab9bc159f60918e405a4e76b2d6d77559
rllola <rllola80@gmail.com>
2020-11-21 10:42:26 +0100

    Setup wallet before test

--- test/wallet.test.js
+++ test/wallet.test.js
@@ -2,12 +2,27 @@ const test = require('ava')
 const Wallet = require('../src/wallet')
 const { getSettings } = require('../src/settings')
 const networks = require('../src/network')
-const bip32 = require('bip32')
+const fs = require('fs')
 
+test.before(t => {
+  // setup wallet files
+  let settings = getSettings(networks.REGTEST)
+
+  if (!fs.existsSync(settings.DATA_FOLDER)) {
+    fs.mkdirSync(settings.DATA_FOLDER, {recursive: true})
+    fs.mkdirSync(path.join(settings.DATA_FOLDER, 'spvnode'))
+    fs.mkdirSync(path.join(settings.DATA_FOLDER, 'wallet'))
+    
+    let wallet = new Wallet(settings)
+    wallet.createSeedFile(mnemonic)
+    wallet.init()
+  }
+  
+})
 
 test('should generate a new address', t => {
-  var settings = getSettings(networks.REGTEST)
-  var wallet = new Wallet(settings)
+  let settings = getSettings(networks.REGTEST)
+  let wallet = new Wallet(settings)
   
   let address = wallet.generateNewAddress()
   
