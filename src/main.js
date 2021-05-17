9cf9a3f8835702e70e0d830b6be8f2c21a067035
rllola <rllola80@gmail.com>
2021-05-17 20:18:16 +0200

    remove hardcoded dev argument

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ const app = require('./app')
 const debug = require('debug')('main')
 
 const network = process.env.NETWORK
+const dev = process.env.DEV
 
-app({ network, dev: true })
+app({ network, dev })
   .catch(function (err) {
     debug(err)
   })
